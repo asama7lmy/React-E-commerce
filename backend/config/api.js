43852fe39c5181adbb0a9d7f779cf035c1b7f1f0
asama7lmy <omarsalahh120@gmail.com>
2025-08-06 17:49:04 +0300

    Final build with correct PUBLIC_URL

--- backend/config/api.js
+++ backend/config/api.js
@@ -1,3 +1,11 @@
+// module.exports = {
+//   rest: {
+//     defaultLimit: 25,
+//     maxLimit: 100,
+//     withCount: true,
+//   },
+// };
+
 module.exports = {
   rest: {
     defaultLimit: 25,
--- backend/config/server.js
+++ backend/config/server.js
@@ -8,10 +8,12 @@
 //     populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
 //   },
 // });
+// 
+
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
-url: env('PUBLIC_URL', 'https://react-e-commerce-with-strapi-final-project-production.up.railway.app'),
+  port: env.int('PORT', 8080), // غيّر إلى 8080 بدل 1337 للمطابقة مع Railway
+  url: env('PUBLIC_URL', 'https://react-e-commerce-with-strapi-final-project-production.up.railway.app'),
   app: {
     keys: env.array('APP_KEYS'),
   },
@@ -19,4 +21,4 @@ url: env('PUBLIC_URL', 'https://react-e-commerce-with-strapi-final-project-produ
     populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
   },
 });
-// 
+
