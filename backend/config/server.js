245e75addaaa717633c20698290c6fcbac95110d
asama7lmy <omarsalahh120@gmail.com>
2025-08-06 18:45:13 +0300

    Fix admin URL to Railway

--- backend/config/server.js
+++ backend/config/server.js
@@ -12,8 +12,8 @@
 
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 8080), // غيّر إلى 8080 بدل 1337 للمطابقة مع Railway
-  url: env('PUBLIC_URL', 'https://react-e-commerce-with-strapi-final-project-production.up.railway.app'),
+  port: env.int('PORT', 8080),
+  url: 'https://react-e-commerce-with-strapi-final-project-production.up.railway.app',
   app: {
     keys: env.array('APP_KEYS'),
   },
@@ -21,4 +21,3 @@ module.exports = ({ env }) => ({
     populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
   },
 });
-
